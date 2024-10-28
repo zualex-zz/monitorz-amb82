bf9b809f49d008d9295eb7a61da33ba1b8e3f78f
Alex Zupan <alex.zupan@gpi.it>
2024-10-28 08:27:01 +0100

    on motion: register video+audio on sdcard

--- monitorz-amb82.ino
+++ monitorz-amb82.ino
@@ -1,23 +1,39 @@
 #include "WiFi.h"
-#include "VideoStream.h"
 #include "StreamIO.h"
+#include "VideoStream.h"
+#include "AudioStream.h"
+#include "AudioEncoder.h"
+#include "MP4Recording.h"
 #include "RTSP.h"
 #include "MotionDetection.h"
 #include "VideoStreamOverlay.h"
 #include "telegramz.h"
 #include "webserverz.h"
 
+// 37,5 x 60 mm board
+
 #define CHANNEL 0      // High resolution video channel for streaming
 #define CHANNELJPEG 1  // jpeg snapshot
 #define CHANNELMD 3    // RGB format video for motion detection only available on channel 3
 
+
+#define GREEN_LED 4
+
+AudioSetting configA(0);
 VideoSetting config(VIDEO_FHD, 30, VIDEO_H264, 0);  // High resolution video for streaming
 VideoSetting configJPG(VIDEO_FHD, CAM_FPS, VIDEO_JPEG, 1);
 VideoSetting configMD(VIDEO_VGA, 10, VIDEO_RGB, 0);  // Low resolution RGB video for motion detection
+
+Audio audio;
+AAC aac;
+MP4Recording mp4;
 RTSP rtsp;
-StreamIO videoStreamer(1, 1);
-StreamIO videoStreamerMD(1, 1);
 MotionDetection MD;
+// StreamIO videoStreamer(1, 1);
+StreamIO videoStreamerMD(1, 1);
+StreamIO audioStreamer(1, 1);      // 1 Input Audio -> 1 Output AAC
+StreamIO avMixStreamer(2, 2);      // 2 Input Video + Audio -> 2 Output MP4 + RTSP
+
 Telegramz *telegramz;
 Webserverz *webserverz;
 
@@ -26,6 +42,9 @@ uint16_t TriggerLimitTime = 2;  // min time between motion detection trigger eve
 
 uint32_t img_addr = 0;
 uint32_t img_len = 0;
+
+bool record = true;
+int recordingCount = 0;
 
 char ssid[] = "ssid";      // your network SSID (name)
 char pass[] = "pass";  // your network password
@@ -39,8 +58,9 @@ void mdPostProcess(std::vector<MotionDetectionResult> md_results) {
 
   OSD.createBitmap(CHANNEL);
   if (MD.getResultCount() > 0) {
-    Serial.print("Motion: ");
-    Serial.println(MD.getResultCount());
+    digitalWrite(GREEN_LED, HIGH);
+    // Serial.print("Motion: ");
+    // Serial.println(MD.getResultCount());
     for (uint16_t i = 0; i < MD.getResultCount(); i++) {
       MotionDetectionResult result = md_results[i];
       int xmin = (int)(result.xMin() * config.width());
@@ -50,13 +70,22 @@ void mdPostProcess(std::vector<MotionDetectionResult> md_results) {
       // printf("%d:\t%d %d %d %d\n\r", i, xmin, xmax, ymin, ymax);
       OSD.drawRect(CHANNEL, xmin, ymin, xmax, ymax, 3, COLOR_GREEN);
     }
-    Camera.getImage(CHANNELJPEG, &img_addr, &img_len);
 
-    if ((unsigned long)(millis() - TRIGGERtimer) >= (TriggerLimitTime * 1000)) {  // limit time between triggers
-      TRIGGERtimer = millis();                                                    // update last trigger time
-      Serial.println("Motion triggered!");
-      // telegramz->send("Motion");
-      telegramz->sendPhotoAs((uint8_t *)img_addr, img_len);
+    if (record) {
+      if (!mp4.getRecordingState()) {    // if not MP4 not in recording mode
+        recordingCount++;
+        mp4.setRecordingFileName("MotionDetection" + String(recordingCount));
+        mp4.begin();
+        // tone(BUZZER_PIN, 1000, 500);
+      }
+    } else {
+      if ((unsigned long)(millis() - TRIGGERtimer) >= (TriggerLimitTime * 1000)) {  // limit time between triggers
+        TRIGGERtimer = millis();                                                    // update last trigger time
+        Serial.println("Motion triggered!");
+        // telegramz->send("Motion");
+        Camera.getImage(CHANNELJPEG, &img_addr, &img_len);
+        telegramz->sendPhotoAs((uint8_t *)img_addr, img_len);
+      }
     }
   }
   OSD.update(CHANNEL);
@@ -64,6 +93,8 @@ void mdPostProcess(std::vector<MotionDetectionResult> md_results) {
 
 void setup() {
   Serial.begin(115200);
+    // GPIO Initialization
+    pinMode(GREEN_LED, OUTPUT);
 
   // attempt to connect to Wifi network:
   while (status != WL_CONNECTED) {
@@ -83,8 +114,15 @@ void setup() {
   Camera.configVideoChannel(CHANNELMD, configMD);
   Camera.videoInit();
 
+    // Configure audio peripheral for audio data output
+    audio.configAudio(configA);
+    audio.begin();
+    // Configure AAC audio encoder
+    aac.configAudio(configA);
+    aac.begin();
   // Configure RTSP for high resolution video stream information
   rtsp.configVideo(config);
+  rtsp.configAudio(configA, CODEC_AAC);
   rtsp.begin();
 
   // Configure motion detection for low resolution RGB video stream
@@ -92,15 +130,30 @@ void setup() {
   MD.setResultCallback(mdPostProcess);
   MD.begin();
 
-  // Configure StreamIO object to stream data from high res video channel to RTSP
-  videoStreamer.registerInput(Camera.getStream(CHANNEL));
-  videoStreamer.registerOutput(rtsp);
-  if (videoStreamer.begin() != 0) {
-    Serial.println("StreamIO link start failed");
-  }
+    // Configure MP4 with identical video format information
+    // Configure MP4 recording settings
+    mp4.configVideo(config);
+    mp4.configAudio(configA, CODEC_AAC);
+    mp4.setRecordingDuration(30);
+    mp4.setRecordingFileCount(1);
+    // Configure StreamIO object to stream data from audio channel to AAC encoder
+    audioStreamer.registerInput(audio);
+    audioStreamer.registerOutput(aac);
+    if (audioStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
 
-  // Start data stream from high resolution video channel
-  Camera.channelBegin(CHANNEL);
+    // Configure StreamIO object to stream data from video channel and AAC encoder to MP4 recording
+    avMixStreamer.registerInput1(Camera.getStream(CHANNEL));
+    avMixStreamer.registerInput2(aac);
+    avMixStreamer.registerOutput1(rtsp);
+    avMixStreamer.registerOutput2(mp4);
+    if (avMixStreamer.begin() != 0) {
+        Serial.println("StreamIO link start failed");
+    }
+
+    // Start data stream from video channel
+    Camera.channelBegin(CHANNEL);
   Serial.println("Video RTSP Streaming Start");
 
   // Configure StreamIO object to stream data from low res video channel to motion detection
